bb3ae9eb90c2e4f7a77c4f0c6d5b8c1cfbcf2b32
Philip Beadle <philip.beadle@live.com.au>
2018-04-29 15:12:27 +1000

    Working on brdiging in sceanrios

--- holochat/dna/genesis/genesis.js
+++ holochat/dna/genesis/genesis.js
@@ -5,9 +5,12 @@ function genesis() {
 }
 
 function bridgeGenesis(side, dna, appData) {
-  debug('HoloChat ' + App.Name + ' Bridged to: DNA: ' + dna + ' appData: ' + appData)
+  debug(getBridges())
+  debug(App.Agent.String + ' HoloChat ' + App.Name + ' Bridged to: DNA: ' + dna + ' appData: ' + appData)
   var Agent = JSON.parse(App.Agent.String.replace(/'/g, '"').slice(0, App.Agent.String.indexOf('}') + 1))
+  debug(Agent.KeyMgtAppDNA)
   var genesisPublicKey = bridge(Agent.KeyMgtAppDNA, 'verify', 'getGenesisPublicKey', '')
+  debug(genesisPublicKey)
   var signature = bridge(Agent.KeyMgtAppDNA, 'verify', 'getSignature', '')
   return verifySignature(signature, 'Revocation Method', genesisPublicKey)
 }
